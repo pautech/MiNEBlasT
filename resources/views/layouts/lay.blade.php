2d0147f1c099c51d59f3273a7f07839b7ee0c1ce
Pautech <pauloduga3679.com>
2024-01-29 17:17:40 +0800

    dashboard is partially done gg hahah

--- resources/views/layouts/lay.blade.php
+++ resources/views/layouts/lay.blade.php
@@ -11,6 +11,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
         <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+        <link href="https://fonts.googleapis.com/css?family=Baloo+2:400,800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
@@ -21,7 +22,7 @@
         href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;600;700&display=swap"
     />
     <!-- Link to your custom CSS file -->
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+
 
 
     <link
@@ -104,17 +105,11 @@
 
   </style>
 </head>
-<body>
+<body  style="background-color: #e7d5b9;">
 
 
-
-    <div class="height-100 ">
-            <main class=" mx-0">
-                <div class="content">
-                    @yield('content')
-                </div>
-            </main>
-    </div>
+@yield('content')
+   
 
 
   
--- resources/views/users/dash.blade.php
+++ resources/views/users/dash.blade.php
@@ -1,36 +1,15 @@
 <!-- resources/views/i_dash.blade.php -->
 
-@extends('layouts.lay')  <!-- Assuming you have a layout file -->
-
+@extends('layouts.lay') 
+@include('layouts.navbar')
 @section('content')
-    <!-- <div class="container">
 
-
-
-
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Instructor Dashboard') }}</div>
-
-                    <div class="card-body"> -->
-                        <!-- <p>Welcome, {{ Auth::guard('web')->user()->username }}!</p> -->
-                        <!-- You can add more content to your dashboard as needed -->
-                        <!-- For example, display instructor-specific information or actions -->
-                    <!-- </div>
-                </div>
-            </div>
-        </div>
-    </div> -->
-
-    <!-- <img class="image" src={{asset('mineblast-final.png')}}  style="width: 250px;" alt="Mineblast Image"/> -->
-
-    @include('layouts.navbar')
-    <div class="bg-info p-4 position-absolute" style="border-top-left-radius: 50px; border-top-right-radius: 50px; border-bottom-right-radius: 50px; width: 300px; font-family:cursive; font-weight: bold; top: 170px; left: 850px;">
-        Greetings, Mining Engineers! This is Engr. Lorry!
-
-        </div>
-        <img class="image mx-auto d-block" src="{{asset('Avatar.png')}} " style="width: 450px; margin-top: 6.8em;" alt="Avatar"/>
+   <div class="container-fluid dashboard d-flex align-items-center justify-content-center">
+   <img class="" src="{{asset('Avatar.png')}} " style="width: 30em;height:auto; margin-top:5em;" alt="Avatar"/>
+   <div class="speech-bubble p-4 shadow-lg"  style=" margin-top:-10em; margin-left:-4em;"><h6>Greetings, Mining Engineers! This is Engr. Lorry! lorem10</h6></div>
+</div>
+                
+   </div>
         
 @endsection
 
