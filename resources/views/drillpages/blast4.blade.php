2826027fd36b56d7a507b0a06add11d85f9d375f
Paulo Duga <pauloduga3679@gmail.com>
2024-04-21 12:29:07 +0800

    blast4 is done

--- resources/views/drillpages/blast4.blade.php
+++ resources/views/drillpages/blast4.blade.php
@@ -12,16 +12,19 @@
     <br>
     <p class="text-justify fs-5">The two methods available include plaster or mud-cap blasting and pop-shooting.</p>
     <div class="d-flex justify-content-center "> </div> 
-    <div class="row ml-5">
-            <div class="col-4">   
-               <p>This method involves firing a charge of explosive placed on the rock and covered with clay (or, a “mud-cap”).</p>
+      <div class="row ml-5 d-flex ">
+            <div class="col-4  d-flex">   
+               <p class="align-self-end">This method involves firing a charge of explosive placed on the rock and covered with clay (or, a “mud-cap”).</p>
             </div>   
             <div class="col">   
               
                 <a class="text-decoration-none text-dark " > <img class="mt-5" src="{{ asset('img/plasting.png') }}" alt="drill picture" style="height:10rem; width:30rem;"></a>
                 
             </div>
-        </div>
+       
+   
+      </div>
+
       <div class=" mt-5 btn d-flex justify-content-end  outline-none  border-0">
          <a class="text-decoration-none text-dark " href="{{ route('blast3') }}"><button type="button" class="rock-btn shadow-lg pl-5 pr-5 ml-3">Next</button></a>
          <a class="text-decoration-none text-dark " href="{{ route('blast4') }}"><button type="button" class="rock-btn shadow-lg pl-5 pr-5 ml-3">Next</button></a>
